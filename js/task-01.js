745d302bab7ca2f0b1ed2c27dd4d98e5aa21fafb
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-05-10 16:11:06 +0300

    added task-02

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
+'use strict';
+
+console.log('================task-01=====================')
+
 // Напиши скрипт, который выполнит следующие операции.
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
@@ -10,7 +14,34 @@ console.log(`В списке ${categories.children.length} категории.`)
 
 categories.querySelectorAll('.item').forEach(item => {
     console.log(`Категория: ${item.querySelector('h2').textContent}`);
-    console.log(
-        `Количество элементов: ${item.querySelector('ul').children.length}`,
-    );
+    console.log(`Количество элементов: ${item.querySelector('ul').children.length}`,);
 });
+
+console.log('================task-02=====================')
+
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
+// после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM-узлов используй document.createElement().
+
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const ul = ingredients.map(item => {
+    console.log(item);
+    const li = document.createElement('li');
+    li.textContent = item;
+    console.log(li);
+    return li; 
+});
+  
+  document.querySelector('#ingredients').append(...ul);
+
+  console.log('================task-03=====================')
+
+  
